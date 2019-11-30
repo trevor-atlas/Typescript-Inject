5c6413fc8c6f2f4aeed38b4be445437bb704591f
Trevor Atlas <trevoristall@gmail.com>
2019-11-30 18:27:30 -0500

    Update Inject.ts

--- src/decorators/Inject.ts
+++ src/decorators/Inject.ts
@@ -4,7 +4,7 @@ export const Inject = (target: any, key: string) => {
     let val = target[key]
 
     if (delete target[key]) {
-        Object.defineProperty(target, key, {
+        return Object.defineProperty(target, key, {
             get: () => {
                 // an ugly cast :(
                 // if you know a good way to do this without converting LazyFactory to an object literal please let me know!
